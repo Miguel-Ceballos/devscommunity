3174df7ea7a4777f718c22ef8fb64f8d61c3aa1c
Miguel Ceballos <mceballosleonides@gmail.com>
2024-01-05 16:28:42 -0600

    Modify register and login styles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,7 +32,7 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-between mt-4">
+        <div class="flex items-center justify-between my-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
@@ -42,6 +42,12 @@
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
+        </div>
+
+        <div class="text-center">
+            <a class="underline text-sm text-gray-400 hover:text-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                {{ __('You do not have an account? Sign up!') }}
+            </a>
         </div>
     </form>
 </x-guest-layout>
