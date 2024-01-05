e6a2ffd243ccecabae91c11ac77deeb2c5a5320e
Miguel Ceballos <mceballosleonides@gmail.com>
2024-01-04 22:09:27 -0600

    Adding functions index and create to PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('posts.index');
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
 }
