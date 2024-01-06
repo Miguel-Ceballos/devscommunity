fa70525d33af02a055b150eeb025e28c36abf260
Miguel Ceballos <mceballosleonides@gmail.com>
2024-01-06 01:04:23 -0600

    Modifying show post styles

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="md:py-4">
+    <div class="md:py-2">
         <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
             @if(session()->has('message'))
                 <p class="p-2 text-green-700 bg-green-50 rounded border-2 border-green-500 mb-2">{{ session('message') }}</p>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,21 +1,40 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Posts') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
+    <div class="md:py-2">
         <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
             <div class="overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <div class="space-y-4">
-                            <div class="bg-white md:rounded-lg space-y-3">
-                                {{ $post->title }}
+                    <div class="bg-white md:rounded-lg space-y-3">
+                        <div class="flex flex-col gap-8 p-4 md:p-8">
+                            <div class="flex gap-2">
+                                <div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                              d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+                                    </svg>
+                                </div>
+                                <div class="flex flex-col">
+                                    <p class="text-md text-gray-700 font-bold">{{ $post->user->name }}</p>
+                                    <p class="text-xs text-gray-500">{{ $post->created_at->diffForHumans() }}</p>
+                                </div>
                             </div>
+                            <p class="text-xl md:text-4xl font-extrabold">{{ $post->title }}</p>
+                            <p class="text-xl">{{ $post->content }}</p>
+                        </div>
+                        <div class="flex gap-2 md:p-8 mt-2 border-t border-gray-200">
+                            <div>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                     stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+                                </svg>
+                            </div>
+                            <x-text-area class="w-full" placeholder="Add a comment"/>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     </div>
 </x-app-layout>
