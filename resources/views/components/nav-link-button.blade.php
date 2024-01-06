1f60be172979d78d3a1339b9bb6938f69494cfb7
Miguel Ceballos <mceballosleonides@gmail.com>
2024-01-05 21:48:21 -0600

    Modifying componentes styles and adding nav-link-button component

---
+++ resources/views/components/nav-link-button.blade.php
@@ -0,0 +1,7 @@
+@php
+    $classes = 'inline-flex items-center rounded-md px-3 py-2 border-2 border-indigo-600 text-sm font-bold leading-5 text-indigo-600 hover:text-white hover:bg-indigo-600 transition duration-150';
+@endphp
+
+<a {{ $attributes->merge(['class' => $classes]) }}>
+    {{ $slot }}
+</a>
