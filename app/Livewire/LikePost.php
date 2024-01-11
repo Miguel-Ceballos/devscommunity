6ccc258cebe131c187b2e29523a6b0ffe6595d6c
Miguel Ceballos <mceballosleonides@gmail.com>
2024-01-11 16:03:49 -0600

    Create migration, controller, model and livewire component to like post

---
+++ app/Livewire/LikePost.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class LikePost extends Component
+{
+    public function render()
+    {
+        return view('livewire.like-post');
+    }
+
+    public function like()
+    {
+        dd('Dando like...');
+    }
+
+}
