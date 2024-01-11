1e2b668fcec775f4b9622c9adf62cca671ca3c02
Miguel Ceballos <mceballosleonides@gmail.com>
2024-01-10 18:32:03 -0600

    Modifying dropdown links

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -34,7 +34,7 @@
                             <x-dropdown-link :href="route('profile.edit')" class="border-b border-gray-300 py-4">
                                 {{ __(Auth::user()->name) }}
                             </x-dropdown-link>
-                            <x-dropdown-link :href="route('dashboard')">
+                            <x-dropdown-link :href="route('posts.index')">
                                 {{ __('Dashboard') }}
                             </x-dropdown-link>
                             <x-dropdown-link :href="route('posts.create')">
@@ -82,7 +82,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+            <x-responsive-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.index')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
