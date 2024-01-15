46f1ba0da74245ed2336ad33025a2bef4a75a2c6
Miguel Ceballos <mceballosleonides@gmail.com>
2024-01-15 13:38:07 -0600

    Modifying query to get posts

--- app/Livewire/ShowPosts.php
+++ app/Livewire/ShowPosts.php
@@ -20,19 +20,24 @@ class ShowPosts extends Component
 
     public function render()
     {
-
-        if ( ! $this->term && auth()->user()->followings->count() !== 0 ) {
-            $ids = auth()->user()->followings->pluck('id')->toArray();
-            array_push($ids, auth()->user()->id);
-            $posts = Post::whereIn('user_id', $ids)->latest()->get();
-        } else if ( ! $this->term && auth()->user()->followings->count() === 0 ) {
-            $posts = Post::all();
+        if ( auth()->user() ) {
+            if ( ! $this->term && auth()->user()->followings->count() !== 0 ) {
+                $ids = auth()->user()->followings->pluck('id')->toArray();
+                array_push($ids, auth()->user()->id);
+                $posts = Post::whereIn('user_id', $ids)->latest()->get();
+            } else if ( ! $this->term && auth()->user()->followings->count() === 0 ) {
+                $posts = Post::all();
+            }
+            else {
+                $posts = Post::when($this->term, function($query) {
+                    $query->where('title', 'LIKE', "%" . $this->term . "%");
+                })->latest()->get();
+            }
         } else {
             $posts = Post::when($this->term, function($query) {
                 $query->where('title', 'LIKE', "%" . $this->term . "%");
-            })->get();
+            })->latest()->get();
         }
-
 
         return view('livewire.show-posts', [
             'posts' => $posts
